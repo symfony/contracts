4d727f81a7eede1a3021fc93f02c1cc7ccb32abd
Nicolas Grekas <nicolas.grekas@gmail.com>
2024-11-28 08:55:08 +0100

    [HttpClient] Fix streaming and redirecting with NoPrivateNetworkHttpClient

--- HttpClient/Test/HttpClientTestCase.php
+++ HttpClient/Test/HttpClientTestCase.php
@@ -36,7 +36,6 @@ abstract class HttpClientTestCase extends TestCase
     {
         TestHttpServer::stop(8067);
         TestHttpServer::stop(8077);
-        TestHttpServer::stop(8087);
     }
 
     abstract protected function getHttpClient(string $testCase): HttpClientInterface;
