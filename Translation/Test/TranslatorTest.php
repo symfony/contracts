888afe4b9606f923340860bb0b856ab9d7a41273
Vincent Langlet <vincentlanglet@hotmail.fr>
2025-07-13 16:04:15 +0200

    [Translation] fix support of `TranslatableInterface` in `IdentityTranslator`

--- Translation/Test/TranslatorTest.php
+++ Translation/Test/TranslatorTest.php
@@ -12,6 +12,7 @@
 namespace Symfony\Contracts\Translation\Test;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Translation\TranslatableMessage;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Contracts\Translation\TranslatorTrait;
 
@@ -116,10 +117,12 @@ class TranslatorTest extends TestCase
 
     public static function getTransTests()
     {
-        return [
-            ['Symfony is great!', 'Symfony is great!', []],
-            ['Symfony is awesome!', 'Symfony is %what%!', ['%what%' => 'awesome']],
-        ];
+        yield ['Symfony is great!', 'Symfony is great!', []];
+        yield ['Symfony is awesome!', 'Symfony is %what%!', ['%what%' => 'awesome']];
+
+        if (class_exists(TranslatableMessage::class)) {
+            yield ['He said "Symfony is awesome!".', 'He said "%what%".', ['%what%' => new TranslatableMessage('Symfony is %what%!', ['%what%' => 'awesome'])]];
+        }
     }
 
     public static function getTransChoiceTests()
