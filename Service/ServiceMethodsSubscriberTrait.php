c2614d2eb7124268b40051c9dd5325b8b121727f
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-12-17 09:14:51 +0100

    Dont use ->getDeclaringClass() when ->class is enough

--- Service/ServiceMethodsSubscriberTrait.php
+++ Service/ServiceMethodsSubscriberTrait.php
@@ -33,7 +33,7 @@ trait ServiceMethodsSubscriberTrait
         $services = method_exists(get_parent_class(self::class) ?: '', __FUNCTION__) ? parent::getSubscribedServices() : [];
 
         foreach ((new \ReflectionClass(self::class))->getMethods() as $method) {
-            if (self::class !== $method->getDeclaringClass()->name) {
+            if (self::class !== $method->class) {
                 continue;
             }
 
